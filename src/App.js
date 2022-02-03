0b1ef37618d4d6f347dc0ab2c44efd5cef71a7ca
vauxc34 <kaxowy12@gmail.com>
2022-02-03 17:03:53 +0100

    front end main site add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
+/* style's */
+
+import './Components/styles/style.css'
+import './Components/styles/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+/* style's */
+
+/* Component's */
+
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+import Footer from './Components/Footer';
+
+/* Component's */
+
 function App() {
   return (
    <>
+
+    <Header/>
+
+
+    <Main/>
+
+    <Footer/>
+
+
    </>
   );
 }
---
+++ src/Components/Main.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+/* component's */
+
+import Slider from './Slider';
+import ShopSection from './ShopSection';
+import AboutSection from './AboutSection';
+import FeatureSection from './FeatureSection';
+import ContactSection from './ContactSection';
+import ClientSection from './ClientSection'
+
+/* component's */
+
+export default function Main() {
+  return (
+      <>
+      
+      <Slider/>
+      <ShopSection/>
+      <AboutSection/>
+      <FeatureSection/>
+      <ContactSection/>
+      <ClientSection/>
+      
+      </>
+  )
+}
